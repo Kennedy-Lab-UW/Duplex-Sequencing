dfae8473e0a2a3e08bba9733c9d536ee77a68a32
bkohrn <kohrnb@u.washington.edu>
2013-09-27 16:33:11 -0700

    updated tag_to_header.py with new options, ect.

--- tag_to_header.py
+++ tag_to_header.py
@@ -1,103 +1,167 @@
-#By Joe Hiatt, Scott Kennedy and Mike Schmitt
-#Version 1.21
-#February 5, 2013
-
 import sys
-from optparse import OptionParser
+from argparse import ArgumentParser
+from Bio import SeqIO
 
-opts = OptionParser()
-opts.add_option('', '--adapter1',  type='string',  default=None,  dest='adapterSeq1')
-opts.add_option('', '--adapter2',  type='string',  default=None,  dest='adapterSeq2')
-opts.add_option('','--infile1',type='string',default=None,dest='infile1')
-opts.add_option('','--infile2',type='string',default=None,dest='infile2')
-opts.add_option('','--outfile1',type='string',default=None,dest='outfile1')
-opts.add_option('','--outfile2',type='string',default=None,dest='outfile2')
-o,args=opts.parse_args()
 
-inFile1 = open(o.infile1,  'r')
-inFile2 = open(o.infile2,  'r')
-outFile1 = open(o.outfile1,  'w')
-outFile2 = open(o.outfile2,  'w')
+parser = ArgumentParser()
+parser.add_argument('--adapter1',  default = None,  dest = 'adapterSeq1')
+parser.add_argument('--adapter2',  default = None,  dest = 'adapterSeq2')
+parser.add_argument('--infile1', default = None, dest = 'infile1')
+parser.add_argument('--infile2', default = None, dest = 'infile2')
+parser.add_argument('--outfile1', default = None, dest = 'outfile1')
+parser.add_argument('--outfile2', default = None, dest = 'outfile2')
+parser.add_argument('--barcode_length', type = int, default = 12, dest = 'blength')
+parser.add_argument('--spacer_length', type = int, default = 5, dest = 'slength')
+parser.add_argument('--read_out', type = int, default = 1000000, dest = 'rOut')
+o=parser.parse_args()
 
- #this is the function that extracts the UID tags from both the forward and reverse read.  Assigns read1 the sequence from some position to the end, then read2 from some position to the end
- #then assigns tag1 from the 5'-end to length of the UID tag for read1 and then read 2.  Change as necessary to your appropriate adapter design.
-tagExtractFxn ='lambda x:(x[0][17:], x[1][17:], x[0][:12], x[1][:12])'
+class fastQRead:
+    def __init__(self, in1, in2, in3, in4):
+        '''This class is meant to hold a single fastQ read.
+        '''
+        self.name=in1.strip().split("@")[1] if "@" in in1 else in1
+        self.seq=in2.strip()
+        self.spacer=""
+        self.qual=in4.strip()
+        if len(self.seq)!=len(self.qual):
+            raise ValueError("Sequence and quality scores of different lengths!/n%s/n%s/n%s/n%s" % (in1, in2, "+", in4))
 
-#this function renames the header with the formatting of *header coordinates,etc*, *tag from read1*, *tag from read2*, *read designation from original header*
-hdrRenameFxn ='lambda x,y,z:"%s#%s%s/%s" % (x.split("#")[0], y, z, x.split("/")[-1])'
+    def __getitem__(self, index):
+        '''This should allow slicing of the read to proceed properly.
+        '''
+        if isinstance(index, int):
+            return self.seq[index]
+        elif isinstance(index, slice):
+            answer = self.__class__(self.name, self.seq[index], self.spacer, self.qual[index])
+            return answer
+        raise ValueError("Invalid index")
 
-tagExtractFxn = eval (tagExtractFxn)
-hdrRenameFxn = eval (hdrRenameFxn)
 
-infilelist = [inFile1,  inFile2] 
-outfilelist = [ outFile1, outFile2 ]
+class fastQItterator:
+    def __init__(self, inFile):
+        '''This class will go through a fastQ file one line at a time.
+        '''
+        self.source=inFile
+        self.eof=False
+    
+    def next(self):
+        newREad=""
+        try:
+            newRead=fastQRead(self.source.next(), self.source.next(), self.source.next(), self.source.next())
+        except Exception:
+            self.eof=True
+            return("EOF")
+        return(newRead)
 
-ctr = 0
-totalreads = 0
+    def close(self):
+        self.source.close()
+        return(True)
+
+
+class fastqWriter:
+    def __init__(self, outFile):
+        self.file=outFile
+        self.firstLine=True
+    
+    def write(self, read):
+        if self.firstLine==True:
+            self.file.write("@" + read.name)
+            self.firstLine=False
+        else:
+            self.file.write("\n@" + read.name)
+        self.file.write("\n" + read.seq)
+        self.file.write("\n" + read.spacer)
+        self.file.write("\n" + read.qual)
+        return(True)
+    
+    def close(self):
+        self.file.close()
+        return(True)
+
+
+def tagExtractFxn(x, blen):
+    '''this is the function that extracts the UID tags from both the 
+    forward and reverse read.  Assigns read1 the sequence from some 
+    position to the end, then read2 from some position to the end, 
+    then assigns tag1 from the 5'-end to length of the UID tag for 
+    read1 and then read 2.
+    '''
+    return(x[0][:blen], x[1][:blen])
+
+
+def hdrRenameFxn(x, y, z):
+    '''this function renames the header with the formatting of 
+    *header coordinates,etc*, *tag from read1*, *tag from read2*, 
+    *read designation from original header*
+    '''
+    return("%s#%s%s/%s" % (x.split("#")[0], y, z, x.split("/")[-1]))
+
+
+in1=fastQItterator(open(o.infile1, 'rU'))
+in2=fastQItterator(open(o.infile2, 'rU'))
+out1=fastqWriter(open(o.outfile1, 'w'))
+out2=fastqWriter(open(o.outfile2, 'w'))
+
+ctr=0
+nospacer = 0
 goodreads = 0
 badtag = 0
-nospacer = 0
+oldBad = 0
+EOF=False
 
-for line1 in infilelist[0]:
-    line1 = line1.strip()
-    line2 = infilelist[1].next().strip()
-
-    ctr += 1
-    
-    if ctr%4==1:
-        hdr1 = line1
-        hdr2 = line2
-
-    elif ctr%4==2:
-        read1 = line1
-        read2 = line2
-
-    elif ctr%4==0:
-        totalreads += 1
-
-        if o.adapterSeq1 != None and o.adapterSeq2 != None and ( read1[12:16] != o.adapterSeq1 or read2[12:16] != o.adapterSeq2 )  : #filtering logic is contained here changes the values as necessary to the location of your adapter spacers (0 indexed)
+while EOF==False:
+    read1 = in1.next()
+    read2 = in2.next()
+    if read1 == "EOF" or read2 == "EOF":
+        EOF==True
+    else:
+        
+        ctr += 1
+        if o.adapterSeq1 != None and o.adapterSeq2 != None and (read1.seq[o.blength:o.blength + o.slength] != o.adapterSeq1 or read2[o.blength:o.blength + o.slength] != o.adapterSeq2):
+    #        sys.stderr.write('Error: something is wrong with the spacers')
+    #        print(read1)
+    #        print(read2)
             nospacer += 1
 
         else :
             #extract tags
-            read1, read2,  tag1,  tag2 = tagExtractFxn((read1, read2))
-            line1, line2,  trash,  trash2 = tagExtractFxn (( line1, line2 ))
+            tag1, tag2 = tagExtractFxn((read1.seq, read2.seq),o.blength)
             
             #header reconstruction
-            hdr1 = hdrRenameFxn ( hdr1, tag1, tag2 ) 
-            hdr2 = hdrRenameFxn ( hdr2, tag1, tag2 )
-            
-            #tag1Hash = int(tag1.count('A')) + int(tag1.count('C')) + int(tag1.count('G')) + int(tag1.count('T'))
-            #tag2Hash = int(tag1.count('A')) + int(tag1.count('C')) + int(tag1.count('G')) + int(tag1.count('T'))
+            read1.name = hdrRenameFxn(read1.name, tag1, tag2) 
+            read2.name = hdrRenameFxn(read2.name, tag1, tag2)
             
             #fastq reconstruction
             if (tag1.isalpha() and tag1.count('N') == 0) and (tag2.isalpha() and tag2.count('N') == 0) :
-                outfilelist[ 0 ].write( "%s\n%s\n+\n%s\n"%( hdr1, read1[4:], line1[4:] ) ) # keeps nucleotides 5 onwards (1-indexed) from read that is already trimmed of UID and adapter spacer
-                outfilelist[ 1 ].write( "%s\n%s\n+\n%s\n"%( hdr2, read2[4:], line2[4:] ) ) # keeps nucleotides 5 onwards (1-indexed) from read that is already trimmed of UID and adapter spacer
-                #outfilelist[ 0 ].write( "%s\n%s\n+\n%s\n"%( hdr1, read1, line1 ) ) # keeps nucleotides 8 onwards (1-indexed) from read that is already trimmed of UID and adapter spacer
-                #outfilelist[ 1 ].write( "%s\n%s\n+\n%s\n"%( hdr2, read2, line2 ) ) # keeps nucleotides 8 onwards (1-indexed) from read that is already trimmed of UID and adapter spacer
-
+                rOut1 = read1[o.blength + o.slength:]
+                rOut2 = read2[o.blength + o.slength:]
+                
+                out1.write(rOut1)
+                out2.write(rOut2)
                 goodreads += 1
+            else: 
+                #sys.stderr.write('Error: something is wrong with the tags')
+                #print(read1)
+                #print(read2)
+                badtag += 1
+        
+            if ctr%o.rOut==0:
+                sys.stderr.write("Total sequences processed: %s\n" % (ctr))
+                sys.stderr.write("Sequences passing filter: %s\n" % (goodreads))
+                sys.stderr.write("Missing spacers: %s\n" % (nospacer))
+                sys.stderr.write("Bad tags: %s\n\n" % (badtag))
+                if badtag == oldBad+o.rOut:
+                    raise IOError("Error between lines %s and %s." % ((ctr-o.rOut)*4,(ctr-o.rOut)*4))
+                else:
+                    oldBad = badtag
 
-            else: badtag += 1
-    
-    if ctr%400000==0:
-        sys.stderr.write( "processed %d reads...\n"%(ctr/4) )
-        print "Total sequences processed:", totalreads
-        print "Sequences passing filter:", goodreads
-        print "Missing spacers:", nospacer
-        print "Bad tags:", badtag
-        continue
-    
-for file in infilelist:
-    file.close()
+in1.close()
+in2.close()
+out1.close()
+out2.close()
 
-for file in outfilelist:
-    file.close()
-
-print " "
-print "Summary statistics:"
-print "Total sequences processed:", totalreads
-print "Sequences passing filter:", goodreads
-print "Missing spacers:", nospacer
-print "Bad tags:", badtag
+sys.stderr.write("/nSummary statistics:\n")
+sys.stderr.write("Total sequences processed: %s\n" % (ctr))
+sys.stderr.write("Sequences passing filter: %s\n" % (goodreads))
+sys.stderr.write("Missing spacers: %s\n" % (nospacer))
+sys.stderr.write("Bad tags: %s" % (badtag))
