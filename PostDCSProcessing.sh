1323bafb629fe1e96588d6b42a514e2c65ca27c3
bkohrn <kohrnb@u.washington.edu>
2014-03-06 11:16:54 -0800

    Update PostDCSProcessing.sh

--- PostDCSProcessing.sh
+++ PostDCSProcessing.sh
@@ -24,17 +24,21 @@ echo ' '
 
 #----------------filter for maping reads---------------
 echo 'Filtering:'
-samtools view -F 4 -b $1 > ${1/.bam/.filt.bam}
+samtools view -F 4 -b $1 > ${1/.aln.sort.bam/.filt.bam}
 
 #----------------clipping final file---------------
 echo 'Clipping Final File:'
-java -jar $picard/AddOrReplaceReadGroups.jar INPUT=${1/.bam/.filt.bam} OUTPUT=${1/.bam/.filt.readgroups.bam} RGLB=UW RGPL=Illumina RGPU=ATATAT RGSM=default
+java -jar $picard/AddOrReplaceReadGroups.jar INPUT=${1/.aln.sort.bam/.filt.bam} OUTPUT=${1/.aln.sort.bam/.filt.readgroups.bam} RGLB=UW RGPL=Illumina RGPU=ATATAT RGSM=default
 
-samtools index ${1/.bam/.filt.readgroups.bam}
+samtools index ${1/.aln.sort.bam/.filt.readgroups.bam}
 
-java -Xmx2g -jar $gaTK/GenomeAnalysisTK.jar -T ClipReads -I ${1/.bam/.filt.readgroups.bam} -o ${1/.bam/.clipped.bam} -R $refGenome --cyclesToTrim "1-5,80-84" --clipRepresentation SOFTCLIP_BASES --fix_misencoded_quality_scores
+java -Xmx2g -jar $gaTK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $refGenome -I ${1/.aln.sort.bam/.filt.readgroups.bam} -o ${1/.aln.sort.bam/.filt.readgroups.intervals}
+
+java -Xmx2g -jar $gaTK/GenomeAnalysisTK.jar -T IndelRealigner -R $refGenome -I ${1/.aln.sort.bam/.filt.readgroups.bam} -targetIntervals ${1/.aln.sort.bam/.filt.readgroups.intervals}  -o ${1/.aln.sort.bam/.filt.readgroups.realign.bam}
+
+java -Xmx2g -jar $gaTK/GenomeAnalysisTK.jar -T ClipReads -I ${1/.aln.sort.bam/.filt.readgroups.bam} -o ${1/.aln.sort.bam/.filt.readgroups.clipped.bam} -R $refGenome --cyclesToTrim "1-4,81-84" --clipRepresentation SOFTCLIP_BASES
 
 #----------------generating stats from final file---------------
-samtools mpileup -B -A -d 500000 -f $refGenome ${1/.bam/.clipped.bam} | tee ${1/.bam/.pileup} | python $progPath/CountMuts.py -d $3 -c $4 -C $5 -u | tee ${1%%.bam}.d${3}-c${4}-${5}.unique.countmuts
+samtools mpileup -B -A -d 500000 -f $refGenome ${1/.aln.sort.bam/.filt.readgroups.clipped.bam} | tee ${1/.aln.sort.bam/.pileup} | python $progPath/CountMuts.py -d $3 -c $4 -C $5 -u | tee ${1%%.aln.sort.bam}.d${3}-c${4}-${5}.unique.countmuts
 
-python $progPath/mut-position.py -i ${1/.bam/.pileup} -o ${1%%.bam}.d${3}-c${4}-${5}.mutpos -d $3 -c $4 -C $5
+python $progPath/mut-position.py -i ${1/.aln.sort.bam/.pileup} -o ${1%%.aln.sort.bam}.d${3}-c${4}-${5}.mutpos -d $3 -c $4 -C $5
