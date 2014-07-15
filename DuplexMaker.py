1fa3f2d55121655de6d2b105d2c756f8d25374d0
bkohrn <kohrnb@u.washington.edu>
2014-07-15 15:10:20 -0700

    set required=True for required args

--- DuplexMaker.py
+++ DuplexMaker.py
@@ -65,8 +65,8 @@ def DSCMaker (groupedReadsList,  readLength) :
 def main():
     # Parameters to be input.
     parser=ArgumentParser()
-    parser.add_argument("--infile", action="store", dest="infile", help="input BAM file", default='sys.stdin')
-    parser.add_argument("--outfile",  action="store", dest="outfile", help="output BAM file",  default='sys.stdout')
+    parser.add_argument("--infile", action="store", dest="infile", help="input BAM file", required=True)
+    parser.add_argument("--outfile",  action="store", dest="outfile", help="output BAM file",  requried=True)
     parser.add_argument('--Ncutoff', type=float, default=1.0, dest='Ncutoff', help="Maximum percentage of Ns allowed in a consensus [1.0]")
     parser.add_argument('--readlength', type=int, default=84, dest='read_length', help="Length of the input read that is being used. [84]")
     parser.add_argument('--barcode_length', type = int, default = 12, dest = 'blength', help = 'Length of the duplex tag sequence. Should match the value in tag_to_header.  [12]')
