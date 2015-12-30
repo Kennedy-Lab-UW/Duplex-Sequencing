b3504781704bf2aacb41ea6d514a1e56275bc09b
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2015-12-31 00:19:05 +0100

    clean.py

--- clean.py
+++ clean.py
@@ -18,15 +18,14 @@ output=os.path.realpath(o.output_folder)
 
 os.chdir(scripts)
 
-files_list=['clean.py','DuplexMaker.py', '.git', 'DCS_family_size_plotter.py', 'PostDCSProcessing.sh', '.idea', 'PE_BASH_MAKER.py', '.gitignore', 'muts_by_read_position.py', 'ConsensusMaker.py', 'bash_template.sh', 'Duplex-Process-Numbers.txt', 'README.md', 'mut-position.py', 'LICENSE', 'tag_to_header.py', 'TestData', 'flag_translations.txt', 'CountMuts.py', 'ProgramOptions.html']
 new_folder=o.output_folder.split("/")
 new_folder=new_folder[len(new_folder)-1]
-files_list.append(new_folder)
 
 new_files=os.listdir(scripts)
-new_files=[ s for s in new_files if s not in files_list ]
+new_files=[ s for s in new_files if new_folder in s]
 
 for s in new_files:
-    shutil.move(s, output+"/"+s)
+    if s != new_folder:
+        shutil.move(s, output+"/"+s)
 
 sys.exit(0)
